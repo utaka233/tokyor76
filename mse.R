499d24db0ae80e18b65e0c5b935fef88003e144d
Takayuki Uchiba <takayuki.uchiba@sugakubunka.com>
2019-04-15 01:16:37 +0900

    Update mse.R

--- mse.R
+++ mse.R
@@ -16,17 +16,17 @@ args <- list(n = rep(size, each = n_iter),
              mean = rep(0.0, each = n_iter),
              sd = rep(10.0, each = n_iter))
 x <- args %>% pmap(rnorm)
-data <- tibble(vars_umvue = vars_umvue <- x %>% map_dbl(var),
+dat <- tibble(vars_umvue = vars_umvue <- x %>% map_dbl(var),
                vars_mse = vars_mse <- x %>% map_dbl(var_mse)) 
 
 # 推定量のヒストグラム
-ggplot(data = data %>% gather(), mapping = aes(x = value, fill = key)) + 
+ggplot(data = dat %>% gather(), mapping = aes(x = value, fill = key)) + 
   geom_histogram(alpha = 0.25, position = "identity", stat = "density") + 
   xlab("母分散の推定値") +
   ylab("密度")
 
 # MSE = bias^2 + std_error^2
-data %>%
+dat %>%
   summarize(bias_umvue = 100.0 - mean(vars_umvue),
             bias_mse = 100.0 - mean(vars_mse),
             error_umvue = sd(vars_umvue),
